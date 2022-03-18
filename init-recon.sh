53d92c403e91d9dfe3a20c30507412a3e99de1f3
KickAss101 <46389158+KickAss101@users.noreply.github.com>
2022-03-18 21:00:18 +0530

    Add files via upload

---
+++ init-recon.sh
@@ -0,0 +1,80 @@
+#! /usr/bin/bash
+
+usage() { echo "Usage: init-recon [-t <target>] (or) [-f <file>]" 1>&2; exit 1; }
+
+# command line Arguments
+getopts t:f:h FLAG;
+case $FLAG in
+    t) t=$OPTARG;;
+    f) f=$OPTARG;;
+    *|h) usage;;
+esac
+
+# Setting flags for file and direct domain | reduces code
+if [ $FLAG = "t" ]; then
+    dir=$t
+    findomain_flag=t
+    subfinder_flag=d
+else
+    dir=$(echo $f | cut -d "." -f 1)
+    findomain_flag=f
+    subfinder_flag=dL
+fi
+
+# Make directories
+if [ ! -d ~/bug-bounty/$dir ];then
+    mkdir ~/bug-bounty/$dir
+fi
+if [ ! -d ~/bug-bounty/$dir/recon ];then
+    mkdir ~/bug-bounty/$dir/recon
+fi
+
+# Copy scope file
+if [ $FLAG = "f" ]; then
+    cp $OPTARG ~/bug-bounty/$dir/recon/
+fi
+# Change directory
+cd ~/bug-bounty/$dir/recon
+tput setaf 42; echo "----------- changed directory -----------";tput setaf 7
+echo
+
+# subdomain enumeration with findomain and subfinder
+findomain-linux -$(echo $findomain_flag) $OPTARG -r -o
+echo
+echo
+sleep 1
+tput setaf 42; echo "[+] subs enum -------> subfinder";tput setaf 7
+echo
+echo
+subfinder -$subfinder_flag $OPTARG -silent -o subfinder.txt
+
+sleep 1
+echo
+tput setaf 42; echo "[+] sorting -------> subs.final";tput setaf 7
+echo
+echo
+sort -u *.txt | tee subs.final
+sleep 1
+
+# clean up
+rm *.txt
+
+# Probing for live domains with httpx
+echo
+echo
+tput setaf 42; echo "[+] Probing for live domains -------> httpx"; tput setaf 7
+cat subs.final | httpx | awk -F "http(|s)://" {'print $2'} | cut  -d "/" -f 1 | tee subs.live
+sleep 1
+
+# Subdomain takeover test with subzy
+echo
+echo
+tput setaf 42; echo "[+] subdomain takeover test -------> subzy"; tput setaf 7
+subzy -targets subs.live -hide_fails
+sleep 1
+
+# Screenshoting with aquatone
+echo
+echo
+tput setaf 42; echo "[+] screenshot -------> Aquatone"; tput setaf 7
+cat subs.live | aquatone
