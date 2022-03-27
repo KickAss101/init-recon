0fc8413edae825655a2e7b8622a82f7676ebe85a
KickAss101 <jaswanthsunkara@protonamil.com>
2022-03-27 19:46:38 +0530

    update

--- init-recon.sh
+++ init-recon.sh
@@ -8,7 +8,7 @@ usage()
     exit 1
 }
 
-# command line Arguments
+######################## command line Arguments ########################
 getopts t:f:h FLAG;
 case $FLAG in
     t) t=$OPTARG;;
@@ -16,18 +16,20 @@ case $FLAG in
     *|h) usage;;
 esac
 
-# Setting flags for file or domain | reduces code
+######################## Setting flags for file or domain | reduces code ########################
 if [ $FLAG = "t" ]; then
     dir=$t
     findomain_flag=t
     subfinder_flag=d
+    amass_flag=d
 else
     dir=$(echo $f | cut -d "." -f 1)
     findomain_flag=f
     subfinder_flag=dL
+    amass_flag=df
 fi
 
-# Make directories
+######################## Make directories ########################
 if [ ! -d ~/bug-bounty ];then
     mkdir ~/bug-bounty
 fi
@@ -38,57 +40,83 @@ if [ ! -d ~/bug-bounty/$dir/recon ];then
     mkdir ~/bug-bounty/$dir/recon
 fi
 
-# Copy scope file
+######################## Copy root domains file ########################
 if [ $FLAG = "f" ]; then
     cp $OPTARG ~/bug-bounty/$dir/recon/
 fi
 
-# Change directory
+######################## Change directory ########################
 cd ~/bug-bounty/$dir/recon
-tput setaf 42; echo "----------- changed directory -----------";tput setaf 7
-echo
-tput setaf 1; echo "------- all .txt files will be deleted in ~/bug-bounty/$dir/recon ------";tput setaf 7
-echo "Cancel if not intented"
+tput setaf 42; echo "----------- changed directory to $(pwd)-----------";tput setaf 7
+echo;echo;echo;
+
+######################## subdomain enumeration with findomain ########################
+mkdir findomain-subs; cd findomain-subs;
+tput setaf 42; echo "[+] subs enum -------> findomain";tput setaf 7
+findomain-linux -$findomain_flag $OPTARG -q -r -o
+sort -u *.txt | tee subs.findomain
+cp subs.findomain ..;cd ..
+echo;echo
 sleep 1
 
-# subdomain enumeration with findomain and subfinder
-findomain-linux -$findomain_flag $OPTARG -r -o
-echo
-echo
-sleep 1
+######################## subdomain enumeration with subfinder ########################
 tput setaf 42; echo "[+] subs enum -------> subfinder";tput setaf 7
-echo
-echo
-subfinder -$subfinder_flag $OPTARG -silent -o subfinder.txt
-
+echo;echo
+subfinder -$subfinder_flag $OPTARG -silent -o subs.subfinder
 sleep 1
-echo
+
+######################## subdomain enumeration with amass ########################
+echo;echo
+tput setaf 42; echo "[+] subs enum -------> Amass";tput setaf 7
+echo;echo
+amass enum -$amass_flag $OPTARG -silent -no-color -dir .
+sleep 1
+
+######################## Sorting subs to single file ########################
+echo;echo
 tput setaf 42; echo "[+] sorting -------> subs.final";tput setaf 7
-echo
-echo
-sort -u *.txt | tee subs.final
-sleep 1
+echo;echo
+sort -u subs.subfinder subs.findomain | tee subs.final
 
-# clean up
-rm *.txt
-
-# Probing for live domains with httpx
-echo
-echo
+######################## Probing for live domains with httpx ########################
+echo;echo
 tput setaf 42; echo "[+] Probing for live domains -------> httpx"; tput setaf 7
-cat subs.final | httpx | awk -F "http(|s)://" {'print $2'} | cut  -d "/" -f 1 | tee subs.live
+echo;echo
+cat subs.final | httpx -silent | awk -F "http(|s)://" {'print $2'} | cut  -d "/" -f 1 | tee subs.live
 sleep 1
 
-# Subdomain takeover test with subzy
-echo
-echo
+######################## Subdomain takeover test with subzy ########################
+echo;echo
 tput setaf 42; echo "[+] subdomain takeover test -------> subzy"; tput setaf 7
+echo;echo
 subzy -targets subs.live -hide_fails
 sleep 1
 
-# Screenshoting with aquatone
-echo
-echo
+######################## Screenshoting with aquatone ########################
+echo;echo
+mkdir aquatone; cd aquatone;
 tput setaf 42; echo "[+] screenshot -------> Aquatone"; tput setaf 7
-echo
-cat subs.live | aquatone
+echo;echo
+cat ../subs.live | aquatone
+cd ..
+sleep 1
+
+######################## Screenshoting with gowitness ########################
+echo;echo
+mkdir gowitness; cd gowitness;
+tput setaf 42; echo "[+] screenshot -------> gowitness"; tput setaf 7
+echo;echo
+gowitness file ../subs.live
+cd ..
+sleep 1
+
+######################## Port Analysis ########################
+masscan
+
+######################## WAF Detection ########################
+if [ $FLAG = "t" ]; then
+    ip=$(host $OPTARG | grep $OPTARG -m 1 | awk -F "$OPTARG has address " {'print $2'})
+    wafwoof $ip
+    echo;echo;echo;
+    nmap $ip --script=http-waf-fingerprint -p80,443
+fi
